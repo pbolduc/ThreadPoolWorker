05fbce83422c25142fb344268b4453c5201aab98
Phil Bolduc <philbolduc@gmail.com>
2022-02-08 17:07:49 -0800

    simple app to monitor threadpool

---
+++ ThreadPoolWeb/Program.cs
@@ -0,0 +1,35 @@
+using Serilog;
+using ThreadPoolWeb;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
+{
+    loggerConfiguration.WriteTo.Console();
+});
+
+builder.Services.AddHostedService<ThreadPoolWorker>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ ThreadPoolWeb/ThreadPoolWorker.cs
@@ -0,0 +1,37 @@
+﻿namespace ThreadPoolWeb
+{
+    public class ThreadPoolWorker : IHostedService
+    {
+        private readonly ILogger<ThreadPoolWorker> _logger;
+
+        public ThreadPoolWorker(ILogger<ThreadPoolWorker> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                ThreadPool.GetMaxThreads(out int maxWorkerThreads, out int maxIoThreads);
+                ThreadPool.GetAvailableThreads(out int freeWorkerThreads, out int freeIoThreads);
+                ThreadPool.GetMinThreads(out int minWorkerThreads, out int minIoThreads);
+
+                _logger.LogInformation("MinWorkerThreads: {MinWorkerThreads}", minWorkerThreads);
+                _logger.LogInformation("MaxWorkerThreads: {MaxWorkerThreads}", maxWorkerThreads);
+                _logger.LogInformation("FreeWorkerThreads: {FreeWorkerThreads}", freeWorkerThreads);
+
+                _logger.LogInformation("MinIoThreads: {MinIoThreads}", minIoThreads);
+                _logger.LogInformation("MaxIoThreads: {MaxIoThreads}", maxIoThreads);
+                _logger.LogInformation("FreeIoThreads: {FreeIoThreads}", freeIoThreads);
+
+                await Task.Delay(TimeSpan.FromSeconds(10));
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
